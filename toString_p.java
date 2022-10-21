362232a5ca3f3c8af86eb24626f9f16c6e2a2559
EducationSH <104467945+EducationSH@users.noreply.github.com>
2022-10-21 23:24:03 +0300

    Delete toString_p.java

--- toString_p.java
+++
@@ -1,9 +0,0 @@
-String s = "Place " + getFullName() + "\n";
-s += "Input transitions:\n";
-for( TimedTransition tr : inputTransitions )
-s += "  " + tr.getFullName() + "\n";
-s += "Output transitions:\n";
-for( TimedTransition tr : outputTransitions )
-	s += "  " + tr.getFullName() + "\n";
-s += "Tokens: " + tokens;
-return s;
